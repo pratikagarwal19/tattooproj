d140060cf9ae89d7875eda04a9a359b1ebb4edff
Pratik Prabhu <prabhupratik27@gmail.com>
2020-03-12 23:51:21 +0530

    Removed errors from settings.py

--- tattooproj/settings.py
+++ tattooproj/settings.py
@@ -76,12 +76,6 @@ WSGI_APPLICATION = 'tattooproj.wsgi.application'
 
 DATABASES = {
     'default': {
-"""
-        'ENGINE': 'django.db.backends.sqlite3',  #SWITCH TO MONGODB
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),"""
-        
-                    client = pymongo.MongoClient("mongodb+srv://Derek:trek6000@cluster0-6udxg.mongodb.net/test?retryWrites=true&w=majority")
-                    db = client.test
     }
 }
 
